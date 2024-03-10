aadd31f86af1849fbdd11176db01fcae0d835d6e
Skryabin Nikita <nekit2271@gmail.com>
2024-03-10 16:26:43 +0300

    Переопределены поля category и genre, управление пользователями вынесено в админку

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 from reviews.models import Category, Genre, Title, Review, Comment
 
 
+admin.site.register(get_user_model())
 admin.site.register(Category)
 admin.site.register(Genre)
 admin.site.register(Title)
