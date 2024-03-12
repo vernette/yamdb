9b4026def7efd7b7617c8aae013114eec4bd715a
vernette <nekit2271@gmail.com>
2024-03-12 14:27:13 +0300

    Замена импорта настроек на встроенный в Django

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django.conf import settings
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
@@ -9,9 +10,6 @@ from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-
-
-import api_yamdb.settings as settings
 
 from reviews.models import Category, Genre, Review, Title, User
 from .filters import TitleFilter
